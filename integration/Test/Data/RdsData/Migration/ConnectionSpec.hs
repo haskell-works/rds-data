ad322bded52d59e8b8380aa6168b97605ed23f23
John Ky <john.ky@iohk.io>
2024-12-29 02:48:24 +1100

    Upgrade to hw-polysemy-0.3.1.1

--- integration/Test/Data/RdsData/Migration/ConnectionSpec.hs
+++ integration/Test/Data/RdsData/Migration/ConnectionSpec.hs
@@ -31,74 +31,84 @@ import           HaskellWorks.Polysemy.Hedgehog
 import           HaskellWorks.Polysemy.Prelude
 import           HaskellWorks.TestContainers.LocalStack
 import           Lens.Micro
+import qualified System.Info                            as OS
 import qualified Test.Tasty                             as Tasty
 import qualified Test.Tasty.Hedgehog                    as H
 import qualified TestContainers.Tasty                   as TC
 
+isWindows :: Bool
+isWindows = OS.os == "mingw32"
+
+isMacos :: Bool
+isMacos = OS.os == "darwin"
+
 -- cabal test rds-data-test --test-options "--pattern \"/RDS integration test/\""
 tasty_rds_integration_test :: Tasty.TestTree
 tasty_rds_integration_test =
-  TC.withContainers (setupContainers' projectDefaultLocalStack) $ \getContainer ->
-    H.testProperty "RDS integration test" $ propertyOnce $ localWorkspace "rds-data" $ runLocalTestEnv getContainer $ do
-      rdsClusterDetails <- createRdsDbCluster "rds_data_migration" getContainer
+  if isWindows || isMacos
+    then Tasty.testGroup "LocalStackSpec skipped on Windows and MacOS" []
+    else
+      TC.withContainers (setupContainers' projectDefaultLocalStack) $ \getContainer ->
+        H.testProperty "RDS integration test" $ propertyOnce $ localWorkspace "rds-data" $ runLocalTestEnv getContainer $ do
+          rdsClusterDetails <- createRdsDbCluster "rds_data_migration" getContainer
 
-      dbClusterArn <- rdsClusterDetails ^. the @"createDbClusterResponse" . the @"dbCluster" . _Just . the @"dbClusterArn"
-        & nothingFail
+          dbClusterArn <- rdsClusterDetails ^. the @"createDbClusterResponse" . the @"dbCluster" . _Just . the @"dbClusterArn"
+            & nothingFail
 
-      runReaderStatementContextFromClusterDetails rdsClusterDetails $ do
-        waitUntilRdsDbClusterAvailable dbClusterArn
-          & trapFail @AWS.Error
-          & jotShowDataLog @AwsLogEntry
+          runReaderStatementContextFromClusterDetails rdsClusterDetails $ do
+            waitUntilRdsDbClusterAvailable dbClusterArn
+              & trapFail @AWS.Error
+              & jotShowDataLog @AwsLogEntry
 
-        initialiseDb
-          & trapFail @RdsDataError
-          & trapFail @AWS.Error
-          & jotShowDataLog @AwsLogEntry
+            initialiseDb
+              & trapFail @RdsDataError
+              & trapFail @AWS.Error
+              & jotShowDataLog @AwsLogEntry
 
-        migrateUp "db/migration.yaml"
-          & trapFail @AWS.Error
-          & trapFail @IOException
-          & trapFail @JsonDecodeError
-          & trapFail @RdsDataError
-          & trapFail @YamlDecodeError
-          & jotShowDataLog
+            migrateUp "db/migration.yaml"
+              & trapFail @AWS.Error
+              & trapFail @IOException
+              & trapFail @JsonDecodeError
+              & trapFail @RdsDataError
+              & trapFail @YamlDecodeError
+              & jotShowDataLog
 
-        upResult <-
-          ( executeStatement $ mconcat
-              [ "SELECT table_name"
-              , "  FROM information_schema.tables"
-              , "  WHERE table_schema = 'public'"
-              , "    AND table_type = 'BASE TABLE';"
-              ]
-          )
-          & trapFail @AWS.Error
-          & trapFail @RdsDataError
-          & jotShowDataLog
+            upResult <-
+              ( executeStatement $ mconcat
+                  [ "SELECT table_name"
+                  , "  FROM information_schema.tables"
+                  , "  WHERE table_schema = 'public'"
+                  , "    AND table_type = 'BASE TABLE';"
+                  ]
+              )
+              & trapFail @AWS.Error
+              & trapFail @RdsDataError
+              & jotShowDataLog
 
-        let upTables = upResult ^.. the @"records" . each . each . each . the @"stringValue" . _Just
+            let upTables = upResult ^.. the @"records" . each . each . each . the @"stringValue" . _Just
 
-        L.sort upTables === ["examples", "migration", "projects", "users"]
+            L.sort upTables === ["examples", "migration", "projects", "users"]
 
-        migrateDown "db/migration.yaml"
-          & trapFail @AWS.Error
-          & trapFail @IOException
-          & trapFail @JsonDecodeError
-          & trapFail @RdsDataError
-          & trapFail @YamlDecodeError
-          & jotShowDataLog
+            migrateDown "db/migration.yaml"
+              & trapFail @AWS.Error
+              & trapFail @IOException
+              & trapFail @JsonDecodeError
+              & trapFail @RdsDataError
+              & trapFail @YamlDecodeError
+              & jotShowDataLog
 
-        downResult <-
-          ( executeStatement $ mconcat
-              [ "SELECT table_name"
-              , "  FROM information_schema.tables"
-              , "  WHERE table_schema = 'public'"
-              , "    AND table_type = 'BASE TABLE'"
-              ]
-          )
-          & trapFail @AWS.Error
-          & trapFail @RdsDataError
-          & jotShowDataLog
+            downResult <-
+              ( executeStatement $ mconcat
+                  [ "SELECT table_name"
+                  , "  FROM information_schema.tables"
+                  , "  WHERE table_schema = 'public'"
+                  , "    AND table_type = 'BASE TABLE'"
+                  ]
+              )
+              & trapFail @AWS.Error
+              & trapFail @RdsDataError
+              & jotShowDataLog
 
-        let downTables = downResult ^.. the @"records" . each . each . each . the @"stringValue" . _Just
+            let downTables = downResult ^.. the @"records" . each . each . each . the @"stringValue" . _Just
 
-        L.sort downTables === ["migration"]
+            L.sort downTables === ["migration"]
